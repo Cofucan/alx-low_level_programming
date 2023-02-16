fdc229c0cdd7b7b11301db35b17eac8dda735d47
cofucan <coolucheofia@gmail.com>
2023-02-16 09:56:08 +0100

    feat: C code to print text using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This is a simple C code,
+ * it prints out a text to stdout
+ * using the puts function
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle\n");
+
+    return (0);
+}
