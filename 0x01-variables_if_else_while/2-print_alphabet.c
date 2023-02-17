6c555f0b33213853d8edac7c7cb961be751a1e04
cofucan <coolucheofia@gmail.com>
2023-02-17 20:17:25 +0100

    feat: C code to print alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * This is a simple C code,
+ * it prints out a text to stdout
+ * using the printf function
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+	int len = strlen(alphabets);
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		putchar(alphabets[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
