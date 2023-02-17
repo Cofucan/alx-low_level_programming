2b27661d94142c0e566e070a8f4e0c1be0d590c4
cofucan <coolucheofia@gmail.com>
2023-02-17 18:13:27 +0100

    fix: Remove the absolute value conversion

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,7 +10,7 @@
  * and prints to stdout,
  * the last digit of the number
  *
- * Return: Exit code 0
+ * Return: Success Code 0
  */
 
 int main(void)
@@ -22,7 +22,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	/* We find the last digit using the modulus operator */
-	l = abs(n % 10);
+	l = n % 10;
 
 	if (l > 5)
 	{
