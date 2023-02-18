fff29fb4ea4d3ff3f40f54538d23fe2533c626df
cofucan <coolucheofia@gmail.com>
2023-02-18 02:07:37 +0100

    feat: C code that prints all numbers from 0 to 9 seperated by comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This is a simple C code,
+ * it prints out all numbers from 0 - 9,
+ * using the putchar function.
+ *
+ * It does this by adding each integer,
+ * to the integer value of the ASCII code for '0' which is 48.
+ * This enables it to print the character representation,
+ * of each integer.
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
