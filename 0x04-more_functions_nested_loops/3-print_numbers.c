14c4e4573ad6c9a197c482e5c03bb77b9a932839
cofucan <coolucheofia@gmail.com>
2023-02-23 16:41:08 +0100

    feat: C function that prints all natural numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+# include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * This is a simple C code,
+ * It prints all numbers from 0 - 9.
+ *
+ * Return: 0
+*/
+
+void print_numbers(void)
+{
+	int u;
+
+	for (u = '0'; u <= '9'; u++)
+	{
+		_putchar(u);
+	}
+	_putchar('\n');
+}
