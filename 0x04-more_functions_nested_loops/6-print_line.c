63dc490daddca81cb79d48b16137e5c3fe65ce4f
cofucan <coolucheofia@gmail.com>
2023-02-23 21:30:19 +0100

    feat: C program that prints out a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+# include "main.h"
+
+/**
+ * print_line - Entry point
+ * @n: Number of times to print the '_'
+ *
+ * This is a simple C code,
+ * It prints the underscore sign '_'
+ * a specified number of times
+ *
+ * Return: nothing
+*/
+
+void print_line(int n)
+{
+	int u;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (u = 1; u <= n; u++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
