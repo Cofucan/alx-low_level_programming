379ec4ba07df2235b83b381092ae573a2cfa5ed1
cofucan <coolucheofia@gmail.com>
2023-02-23 16:13:21 +0100

    feat: C function that multiplies 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+
+# include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: First number to be multiplied
+ * @b: Second number to be multiplied
+ *
+ * This is a simple C code,
+ * It returns the product of 2 numbers.
+ *
+ * Return: integer
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
