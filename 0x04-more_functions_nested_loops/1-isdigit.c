0710c649a6239c31368d09c76d0b67d5db2fa725
cofucan <coolucheofia@gmail.com>
2023-02-23 16:03:05 +0100

    feat: C code that checks if char is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+
+# include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * @c: Number to be checked
+ *
+ * This is a simple C code,
+ * it checks if a number is a digit.
+ *
+ * Return: 0 or 1
+*/
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
