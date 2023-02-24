3331c219c99a19b6bb7d656ca09c6552e997aefc
cofucan <coolucheofia@gmail.com>
2023-02-24 02:20:01 +0100

    fix: Remove sqrt function

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -4,28 +4,33 @@
 /**
  * main - Entry point
  *
- * This is a simple C program.
+ * This is a C program.
  * It prints the largest prime factor.
  * of a very large number.
  *
  * Return: Exactly 0.
 */
 
-int main() {
-    long long int n = 612852475143;
-    long long int max_factor = -1;
-    long long int i = 2;
-    while (i * i <= n) {
-        if (n % i == 0) {
-            max_factor = i;
-            n /= i;
-        } else {
-            i++;
-        }
-    }
-    if (n > 1) {
-        max_factor = n;
-    }
-    printf("%lld\n", max_factor);
-    return 0;
+int main(void)
+{
+	long int num, lower, check;
+
+	num = 612852475143;
+	lower = 2;
+	check = 2;
+
+	while (num > check)
+	{
+		if (num % check == 0)
+		{
+			lower = check;
+			num = num / lower;
+			check = lower + 1;
+		}
+		else
+			check++;
+	}
+	printf("%ld\n", num);
+
+	return (0);
 }
