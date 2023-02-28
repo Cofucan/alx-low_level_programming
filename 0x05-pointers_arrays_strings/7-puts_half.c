36ccf97726c7ad1c55eda62bedcded26d31324ff
cofucan <coolucheofia@gmail.com>
2023-02-28 01:28:22 +0100

    feat: C program that prints the second half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * puts_half - Entry point
+ * @str: String to be printed.
+ *
+ * This program prints second half of
+ * the characters of a string
+ * from a character pointer,
+ *
+ * return: Nothing
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int half = 0;
+
+	len = _strlen(str); /* This includes the null terminator */
+	half = (len + 1) / 2;
+	while (half <= (len - 1))
+	{
+		_putchar(*(str + half));
+		half++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - Entry point
+ * @s: String to be counted
+ *
+ * This program counts the length of
+ * a string by counting each character
+ * until the null character is encountered.
+ *
+ * Return: Number of characters in the string.
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+
+	return (count);
+}
