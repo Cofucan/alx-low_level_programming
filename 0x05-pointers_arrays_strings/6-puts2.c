af482288f1e6569507458be1d42395bd342c27bc
cofucan <coolucheofia@gmail.com>
2023-02-28 01:03:10 +0100

    feat: C program that prints every other character in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts2 - Entry point
+ * @str: String to be printed.
+ *
+ * This program prints every other character
+ * of a string from a character pointer.
+ *
+ * return: Nothing
+ */
+
+void puts2(char *str)
+{
+	int c = 0;
+
+	while (*(str + c) != '\0')
+	{
+		if (c % 2 == 0)
+			_putchar(*(str + c));
+		c++;
+	}
+	_putchar('\n');
+}
