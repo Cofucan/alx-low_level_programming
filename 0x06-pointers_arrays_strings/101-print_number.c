470c528c6b790d8c231898dbc60bf8c40492190e
cofucan <coolucheofia@gmail.com>
2023-03-02 13:05:13 +0100

    feat: C program that an integer using the putchar function

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - Entry point
+ * @n: Number to be printed..
+ *
+ * This program prints an integer
+ * using the putchar function.
+ *
+ * Return: Nothing
+ */
+
+void print_number(int n)
+{
+	int n_copy, digit, power, len;
+
+	power = 1;
+	len = 1;
+
+	if (n < 0)
+	{
+		n_copy = n * -1;
+		_putchar('-');
+	}
+	else
+		n_copy = n;
+
+	while (n_copy / power > 9)
+	{
+		power *= 10;
+		len++;
+	}
+
+	while (power >= 10)
+	{
+		digit = n_copy / power;
+		_putchar(digit + '0');
+		n_copy %= power;
+		power /= 10;
+	}
+	_putchar(n_copy + '0');
+}
