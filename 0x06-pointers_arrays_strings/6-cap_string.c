1b4ffdeca597b158ff96bee0d3ed652c6912a073
cofucan <coolucheofia@gmail.com>
2023-03-02 07:08:17 +0100

    feat: C program that capitalizes all words in a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * cap_string - Entry point
+ * @str: Pointer to character array.
+ *
+ * This function capitalizes all words in a string.
+ *
+ * Return: Pointer to string.
+ */
+
+char *cap_string(char *str)
+{
+	int c = 0;
+
+	while (str[c] != '\0')
+	{
+		if (str[c] >= 'a' && str[c] <= 'z' && is_seperator(str[c - 1]))
+		{
+			str[c] -= 32;
+		}
+		c++;
+	}
+	return (str);
+}
+
+
+/**
+ * is_seperator - Utility function
+ * @s: Character to be checked.
+ *
+ * This function checks to see if a character is
+ * a special character by comparing with specific
+ * characters.
+ *
+ * Return: 1 or 0.
+ */
+
+int is_seperator(char s)
+{
+	if (s == '\t' ||
+		s == '\n' ||
+		s == ' ' ||
+		s == ',' ||
+		s == ';' ||
+		s == '.' ||
+		s == '!' ||
+		s == '(' ||
+		s == ')' ||
+		s == '{' ||
+		s == '}'
+	)
+		return (1);
+	else
+		return (0);
+}
