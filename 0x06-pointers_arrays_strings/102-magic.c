2bd4a77dbec9d7fa8370077eb75cd714eec4a21b
cofucan <coolucheofia@gmail.com>
2023-03-02 15:10:50 +0100

    fix

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(&n + sizeof(int) + (sizeof(int) / 4)) = 98;
+  *(&n + sizeof(n) + (sizeof(n) / 4)) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
