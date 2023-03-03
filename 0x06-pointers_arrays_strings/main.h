3c777b5232bdcb2e7276b62241f63d677d727738
cofucan <coolucheofia@gmail.com>
2023-03-03 01:54:51 +0100

    chore: Include prototypes for more functions

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -27,3 +27,7 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 void print_buffer(char *b, int size);
+
+int arr_len(char *str);
+
+void rev_array(char *n);
