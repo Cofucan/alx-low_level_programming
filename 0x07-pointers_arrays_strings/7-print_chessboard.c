c2a45f24c6698e612358c3390e307faddfc65eef
cofucan <coolucheofia@gmail.com>
2023-03-07 04:16:59 +0100

    C program that prints a chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Entry point
+ * @a: 2D array to print.
+ *
+ * This function prints a chessboard.
+ *
+ * Return: Void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int u, v;
+
+	for (u = 0; *a[u]; u++)
+	{
+		for (v = 0; v < 8; v++)
+		{
+			_putchar(a[u][v]);
+		}
+		_putchar('\n');
+	}
+}
