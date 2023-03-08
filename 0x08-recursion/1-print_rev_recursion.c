e8d9d780c89250f37d35edec1cedba8d33f99837
cofucan <coolucheofia@gmail.com>
2023-03-08 10:43:45 +0100

    feat: C program that uses recursion to print a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Entry point.
+ * @s: String to print in reverse.
+ *
+ * This is a function that prints a string,
+ * in reverse, using recursion.
+ *
+ * Return: Void.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
