96a181d2792e6012dab4c3d427135c746695b933
cofucan <coolucheofia@gmail.com>
2023-03-11 01:24:43 +0100

    fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <stdbool.h>
 #include "main.h"
+
+bool is_num(char *num);
 
 /**
 * main - add positive numbers
@@ -23,7 +26,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (isalpha(*argv[i]) != 0)
+		if (!is_num(argv[i]))
 		{
 			printf("%s\n", "Error");
 			return (1);
@@ -34,4 +37,27 @@ int main(int argc, char *argv[])
 	printf("%d\n", sum);
 
 	exit(EXIT_SUCCESS);
+}
+
+/**
+ * is_num - Function
+ * @num: String to check if its a number
+ *
+ * Checks if a string contains only digits.
+ *
+ * Return: true or false.
+ */
+
+bool is_num(char *num)
+{
+	if (*num == '-') /* If string starts with a minus sign, go on to next char */
+		num++;
+
+	while (*num)
+	{
+		if (!isdigit(*num))
+			return (false);
+		num++;
+	}
+	return (true);
 }
