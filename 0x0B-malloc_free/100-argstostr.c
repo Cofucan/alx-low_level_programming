3bccc67fc1c892b393b2a616a70e1960f7b3fe20
cofucan <coolucheofia@gmail.com>
2023-03-14 09:10:29 +0100

    fix: Also include the name of the program in arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -15,14 +15,13 @@ char *argstostr(int ac, char **av)
 	int a, b, x;
 	int size = 0;
 	char *new_string;
-	int new_lines = ac - 1; /* No of bytes to allocate for newlines */
 
 	/* If no argument is passed */
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	/* Count total size of arguments string vector */
-	for (a = 1; av[a]; a++)
+	for (a = 0; av[a]; a++)
 		for (b = 0; av[a][b]; b++)
 			size++;
 
@@ -30,14 +29,14 @@ char *argstostr(int ac, char **av)
 	 * Allocate memory for new string that contains all arguments,
 	 * including space for newline character after each argument.
 	 */
-	new_string = malloc((sizeof(char) * size) + (new_lines * 2));
+	new_string = malloc((sizeof(char) * size) + ac + 1);
 
 	/* If malloc fails */
 	if (!new_string)
 		return (NULL);
 
 	/* Copy every character in every argument passed, followed by newline. */
-	for (a = 1, x = 0; av[a]; a++, x++)
+	for (a = 0, x = 0; av[a]; a++, x++)
 	{
 		for (b = 0; av[a][b]; b++, x++)
 			new_string[x] = av[a][b];
