1b7737d6c523ebcce5dcf71a4f8ec87e2e4e4599
cofucan <coolucheofia@gmail.com>
2023-03-18 08:41:48 +0100

    fix

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,153 +2,171 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void _print(char *str, int l);
-char *mul(char n, char *num, int num_index, char *dest, int dest_index);
-int check_for_digits(char **av);
-void init(char *str, int l);
-/**
- * _print - moves a string one place to the left and prints the string
- * @str: string to move
- * @l: size of string
- *
- * Return: void
- */
-void _print(char *str, int l)
-{
-	int i, j;
-
-	i = j = 0;
-	while (i < l)
-	{
-		if (str[i] != '0')
-			j = 1;
-		if (j || i == l - 1)
-			_putchar(str[i]);
-		i++;
-	}
-
-	_putchar('\n');
-	free(str);
-}
+void multiply(char *num1, int len1, char *num2, int len2, char *arr);
+int only_digits(char *num);
+int _strlen(char *str);
+void _memset(char *arr, int size);
+void _print(char *arr);
 
 /**
- * mul - multiplies a char with a string and places the answer into dest
- * @n: char to multiply
- * @num: string to multiply
- * @num_index: last non NULL index of num
- * @dest: destination of multiplication
- * @dest_index: highest index to start addition
+ * main - Entry point
+ * @argc: Number of arguments passed
+ * @argv: Vector containing arguments passed as strings
  *
- * Return: pointer to dest, or NULL on failure
- */
-char *mul(char n, char *num, int num_index, char *dest, int dest_index)
-{
-	int j, k, mul, mulrem, add, addrem;
-
-	mulrem = addrem = 0;
-	for (j = num_index, k = dest_index; j >= 0; j--, k--)
-	{
-		mul = (n - '0') * (num[j] - '0') + mulrem;
-		mulrem = mul / 10;
-		add = (dest[k] - '0') + (mul % 10) + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	for (addrem += mulrem; k >= 0 && addrem; k--)
-	{
-		add = (dest[k] - '0') + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	if (addrem)
-	{
-		return (NULL);
-	}
-	return (dest);
-}
-
-/**
- * check_for_digits - checks the arguments to ensure they are digits
- * @av: pointer to arguments
- *
- * Return: 0 if digits, 1 if not
- */
-int check_for_digits(char **av)
-{
-	int i, j;
-
-	for (i = 1; i < 3; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-		{
-			if (av[i][j] < '0' || av[i][j] > '9')
-				return (1);
-		}
-	}
-	return (0);
-}
-
-/**
- * init - initializes a string
- * @str: sting to initialize
- * @l: length of strinf
- *
- * Return: void
- */
-void init(char *str, int l)
-{
-	int i;
-
-	for (i = 0; i < l; i++)
-		str[i] = '0';
-	str[i] = '\0';
-}
-
-/**
- * main - multiply two numbers
- * @argc: number of arguments
- * @argv: argument vector
- *
- * Return: zero, or exit status of 98 if failure
+ * Return: Exit status 0 or 98.
  */
 
 int main(int argc, char *argv[])
 {
-	int l1, l2, ln, ti, i;
-	char *a;
-	char *t;
-	char e[] = "Error\n";
+	int c, len1, len2;
+	char *num1, *num2, *result;
+	char err[] = "Error\n";
 
-	if (argc != 3 || check_for_digits(argv))
+	/* If number of args is not exactly 2 or either arguments are not valid */
+	if (argc != 3 || !only_digits(argv[1]) || !only_digits(argv[2]))
 	{
-		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
+		for (c = 0; err[c]; c++)
+			_putchar(err[c]);
 		exit(98);
 	}
-	for (l1 = 0; argv[1][l1]; l1++)
-		;
-	for (l2 = 0; argv[2][l2]; l2++)
-		;
-	ln = l1 + l2 + 1;
-	a = malloc(ln * sizeof(char));
-	if (a == NULL)
+
+	num1 = argv[1];
+	num2 = argv[2];
+
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
+
+	/* If either argument is 0, print 0 */
+	if ((len1 == 1 && *num1 == '0') || (len2 == 1 && *num2 == '0'))
 	{
-		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
+		_putchar('0');
+		_putchar('\n');
+		return (0);
+	}
+
+	/* Allocate memory for the result array */
+	result = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (result == NULL)
+	{
+		for (c = 0; err[c]; c++)
+			_putchar(err[c]);
 		exit(98);
 	}
-	init(a, ln - 1);
-	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
-	{
-		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
-		if (t == NULL)
-		{
-			for (ti = 0; e[ti]; ti++)
-				_putchar(e[ti]);
-			free(a);
-			exit(98);
-		}
-	}
-	_print(a, ln - 1);
+
+	/* Initialize the result array with zeros */
+	_memset(result, len1 + len2);
+
+	/* Multiply the two numbers digit by digit */
+	multiply(num1, len1, num2, len2, result);
+
+	/* Print the result */
+	_print(result);
+
+	/* Free the memory allocated for the result array */
+	free(result);
+
 	return (0);
 }
+
+/**
+ * multiply - Multiplies two numbers stored as character arrays
+ * @num1: Number to multiply
+ * @num2: Number to multiply with
+ * @len1: Length of first number
+ * @len2: Length of second number
+ * @arr: Address to store result of multiplication
+ *
+ * Return: Address containing result of characters for each digit
+ */
+
+void multiply(char *num1, int len1, char *num2, int len2, char *arr)
+{
+	int i, j;
+	int carry, temp;
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			temp = (num1[i] - '0') * (num2[j] - '0') + carry + (arr[i + j + 1] - '0');
+			carry = temp / 10;
+			arr[i + j + 1] = temp % 10 + '0';
+		}
+		arr[i] += carry;
+	}
+}
+
+/**
+ * only_digits - Checks to see if a string contains only numeric characters
+ * @num: Array to check
+ *
+ * Return: 1 or 0
+ */
+
+int only_digits(char *num)
+{
+	int c;
+
+	for (c = 0; num[c]; c++)
+		if (num[c] < '0' || num[c] > '9')
+			return (0);
+	return (1);
+}
+
+/**
+ * _strlen - Counts the number of of elements in an array
+ * @str: Array to check
+ *
+ * Return: Length of array
+ */
+
+int _strlen(char *str)
+{
+	int c;
+
+	for (c = 0; str[c]; c++)
+		continue;
+	return (c);
+}
+
+/**
+ * _memset - Sets all bytes in an array to 0
+ * @arr: Array to initialize
+ * @size: Size of array to be initialized
+ *
+ * Return: Void
+ */
+
+void _memset(char *arr, int size)
+{
+	int m;
+
+	for (m = 0; m < size; m++)
+		arr[m] = '0';
+	arr[m] = '\0';
+}
+
+/**
+ * _print - Prints all characters starting with the first non-zero character
+ * @arr: Array to print
+ *
+ * Return: Void
+ */
+
+void _print(char *arr)
+{
+	int c = 0;
+	char *start = arr;
+
+	while (start[c] == '0')
+		c++;
+
+	while (arr[c])
+	{
+		_putchar(arr[c]);
+		c++;
+	}
+	_putchar('\n');
+}
+
