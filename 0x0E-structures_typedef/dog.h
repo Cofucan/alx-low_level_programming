5d386cc82d90a1087c97555e89216e9f17362f6b
cofucan <coolucheofia@gmail.com>
2023-03-21 00:04:08 +0100

    style: Betty compliant

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure with properties for a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ */
+
 struct dog
 {
 	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 
 #endif /* DOG_H */
