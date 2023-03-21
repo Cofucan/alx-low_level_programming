fbe2e619ee65a20749854b4e78f1f224339d8195
cofucan <coolucheofia@gmail.com>
2023-03-21 02:24:39 +0100

    feat: Prints name of program it was compiled in

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Prints the name of the program it is compiled from.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
