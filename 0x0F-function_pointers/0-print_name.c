a61662ec3d12f6d48b0b11ed2420dde7e899483c
cofucan <coolucheofia@gmail.com>
2023-03-22 22:27:44 +0100

    feat: Function that prints a name using pointer to another function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: Name to be printed
+ * @f: Pointer to function to be used
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
