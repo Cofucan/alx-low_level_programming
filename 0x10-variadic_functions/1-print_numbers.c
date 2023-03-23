4de864570f5d147817aaa6121f2793e6b74f874e
cofucan <coolucheofia@gmail.com>
2023-03-24 00:54:38 +0100

    feat: Prints all numbers passed to function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Prints all numbers passed to function
+ * @separator: Seperator to be printed between numbers
+ * @n: Number of arguments to be printed
+ *
+ * Return: Nothing.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int x;
+	va_list ptr;
+
+	va_start(ptr, n);
+
+	for (x = 0; x < n; x++)
+	{
+		printf("%d", va_arg(ptr, int));
+		if (x != (n - 1)) /* If x is not the last index, print seperator */
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(ptr);
+
+}
