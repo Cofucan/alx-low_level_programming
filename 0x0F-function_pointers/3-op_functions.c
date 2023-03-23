bbd6dd2ad949a38c0ca92c1062940710cbefd896
cofucan <coolucheofia@gmail.com>
2023-03-23 02:53:22 +0100

    feat: Functions for arithmetic operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Sum (integer).
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Finds difference between two integers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Difference (integer).
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Product (integer).
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides a number by another number
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Sum (integer).
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Returns the modulo of two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Remainder (integer).
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
