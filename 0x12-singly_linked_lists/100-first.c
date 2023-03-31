f03830c7e0480475ed3c7f62895e2d4916582498
cofucan <coolucheofia@gmail.com>
2023-03-31 02:28:28 +0100

    feat: Function that executes before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_first - constructor function that executes before the main function
+ *
+ * Return: Nothing.
+ */
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
