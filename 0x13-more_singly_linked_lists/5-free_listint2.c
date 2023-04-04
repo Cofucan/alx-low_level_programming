1e04642f13a5557b7889de3bfbb940b29efd2d28
cofucan <coolucheofia@gmail.com>
2023-04-04 08:09:39 +0100

    feat: Frees a linked list and sets the head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * free_listint2 - frees a list of dynamically allocated structs
+ *					and sets the head to null.
+ * @head: Pointer to the first node
+ *
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *prev;
+	listint_t *curr;
+
+	/* If head points to null, then there's nothing to free */
+	if (!(*head))
+		return;
+
+	prev = *head;
+	curr = *head;
+
+	while (curr)
+	{
+		curr = curr->next;
+		free(prev);
+		prev = curr;
+	}
+
+	/* Set head to null after freeing all the nodes */
+	*head = NULL;
+}
