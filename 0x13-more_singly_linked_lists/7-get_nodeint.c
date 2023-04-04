b6868821faab6402f5cf0f06eb7ac1807457e5b4
cofucan <coolucheofia@gmail.com>
2023-04-05 00:20:03 +0100

    fix: Return null if node not found

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -22,8 +22,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (index == 0)
 		return (head);
 
-	for (u = 0; (node_to_find == NULL || u < index); u++)
+	for (u = 0; (u < index); u++)
+	{
+		/* If we get to the end of the list before the getting to the index */
+		if (!node_to_find)
+			break;
+
 		node_to_find = node_to_find->next;
+	}
 
 	return (node_to_find);
 }
