0fc26e31d26d43c291c34dd19a227986334b5c42
cofucan <coolucheofia@gmail.com>
2023-04-04 23:48:58 +0100

    feat: Gets the node at a specified index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * get_nodeint_at_index - returns the nth node of a singly linked list
+ * @head: Pointer to the first node
+ * @index: The index of the node to find
+ *
+ * Return: Address of the node.
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node_to_find = head;
+	unsigned int u;
+
+	if (!head)
+		return (NULL);
+
+	if (index == 0)
+		return (head);
+
+	for (u = 0; (node_to_find == NULL || u < index); u++)
+		node_to_find = node_to_find->next;
+
+	return (node_to_find);
+}
