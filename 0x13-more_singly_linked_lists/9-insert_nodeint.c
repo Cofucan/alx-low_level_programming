9e48569f9383ccba3506dc8f827f777cc8e28fa9
cofucan <coolucheofia@gmail.com>
2023-04-05 03:27:21 +0100

    fix: If list is empty and index is 0, node should be added

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int x = 0;
 
 	/* If list is empty */
-	if (!(*head))
+	if (!(*head) && idx)
 		return (NULL);
 
 	/* If index is 0, add the new node at the beginning of the list */
