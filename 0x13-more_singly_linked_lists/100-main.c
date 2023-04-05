8008587c903c94756772b1ad4f7fe3ff1c9ad3e0
cofucan <coolucheofia@gmail.com>
2023-04-05 04:14:44 +0100

    test: Test files for advanced tasks

--- 0x13-more_singly_linked_lists/100-main.c
+++ 0x13-more_singly_linked_lists/100-main.c
@@ -22,16 +22,8 @@ int main(void)
     add_nodeint_end(&head, 402);
     add_nodeint_end(&head, 1024);
     print_listint(head);
-    printf("-----------------\n");
-    insert_nodeint_at_index(&head, 5, 4096);
-    print_listint(head);
+    reverse_listint(&head);
+    print_listint(head);    
     free_listint2(&head);
     return (0);
 }
-
-/**
- * Edge cases to consider:
- * Index greater than the length of the list should not be added
- * If index is the null pointer index, node should be added at the end
- * If list is empty and index is 0, node should be added as the first node
- */
