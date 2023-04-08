68da995bba8053c168ac42b4f88cfba738248a32
cofucan <coolucheofia@gmail.com>
2023-04-08 06:34:02 +0100

    style: Betty compliant

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -12,17 +12,17 @@ int get_endianness(void)
 {
 	unsigned int data = 0x98765432;
 	unsigned char *bytes = (unsigned char *)&data;
-	
+
 	if
-		((bytes[0] == 0x98) && \
-		(bytes[1] == 0x76) && \
-		(bytes[2] == 0x54) && \
+		((bytes[0] == 0x98) &&
+		(bytes[1] == 0x76) &&
+		(bytes[2] == 0x54) &&
 		(bytes[3] == 0x32))
 		return (0);
 	else if
-		((bytes[0] == 0x32) && \
-		(bytes[1] == 0x54) && \
-		(bytes[2] == 0x76) && \
+		((bytes[0] == 0x32) &&
+		(bytes[1] == 0x54) &&
+		(bytes[2] == 0x76) &&
 		(bytes[3] == 0x98))
 		return (1);
 
