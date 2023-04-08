5d1e4216c2e9c4ae62ea68f5eb486595dc0b83e5
cofucan <coolucheofia@gmail.com>
2023-04-08 03:27:11 +0100

    refactor: Remove unnecessary loop

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -19,10 +19,7 @@ int get_bit(unsigned long int n, unsigned int index)
 		return (-1);
 
 	mask = 1UL;
-
-	for (idx = 0; idx < index; idx++)
-		mask <<= 1;
-
+	mask <<= index;
 	bit = (n & mask ? 1 : 0);
 
 	return (bit);
