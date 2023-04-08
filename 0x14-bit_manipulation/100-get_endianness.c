d4df7f8a65a0913a8528578a955a75dd209d539f
cofucan <coolucheofia@gmail.com>
2023-04-08 06:30:27 +0100

    Checks for endianness of the system it is run in

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_endianness - checks the order in which the bytes of a
+ *					multibyte data type are stored in memory
+ *
+ * Return: 0 if big endian or 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int data = 0x98765432;
+	unsigned char *bytes = (unsigned char *)&data;
+	
+	if
+		((bytes[0] == 0x98) && \
+		(bytes[1] == 0x76) && \
+		(bytes[2] == 0x54) && \
+		(bytes[3] == 0x32))
+		return (0);
+	else if
+		((bytes[0] == 0x32) && \
+		(bytes[1] == 0x54) && \
+		(bytes[2] == 0x76) && \
+		(bytes[3] == 0x98))
+		return (1);
+
+	return (-1);
+}
