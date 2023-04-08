440eaa69226cb3ec820d289564514010b18c8669
cofucan <coolucheofia@gmail.com>
2023-04-08 03:10:52 +0100

    feat: Gets the bit at a certain index of a binary number

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "main.h"
+
+/**
+ * get_bit - gets the bit at a certain index of a binary number
+ * @n: Number to be scanned
+ * @index: Index in which to find the bit
+ *
+ * Return: Bit at that index or -1 if index is out of range.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask, idx;
+	bool bit;
+
+	if (index < 0 || index >= (sizeof(unsigned long int) * 8))
+		mask = 1UL;
+
+	for (idx = 0; idx < index; idx++)
+		mask <<= 1;
+
+	bit = (n & mask ? 1 : 0);
+
+	return (bit);
+}
