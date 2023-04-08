196eadc2be396ebb1224aa59c340edc8852c153c
cofucan <coolucheofia@gmail.com>
2023-04-08 05:49:44 +0100

    style: Betty compliant

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,16 +13,16 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int mask, total_bits;
-    unsigned int exclusive_bits = 0;
+	unsigned long int mask, total_bits;
+	unsigned int exclusive_bits = 0;
 
-    total_bits = (sizeof(unsigned long int) * 8);
+	total_bits = (sizeof(unsigned long int) * 8);
 
-    for (mask = 1UL; total_bits; mask <<= 1, total_bits--)
-    {
-        if ((n & mask) ^ (m & mask))
-            exclusive_bits += 1;
-    }
+	for (mask = 1UL; total_bits; mask <<= 1, total_bits--)
+	{
+		if ((n & mask) ^ (m & mask))
+			exclusive_bits += 1;
+	}
 
-    return (exclusive_bits);
+	return (exclusive_bits);
 }
