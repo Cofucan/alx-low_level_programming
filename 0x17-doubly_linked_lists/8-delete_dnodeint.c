b54f6e02b4d8f33074d63749f80720104a5a8e41
cofucan <coolucheofia@gmail.com>
2023-05-19 00:48:54 +0100

    fix: Set previous pointer to NULL after freeing prev node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,7 +19,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{ /* Delete first node if index is 0 */
 		curr = curr->next;
-		curr ? free(curr->prev) : free(*head);
+		free(*head);
+		curr ? curr->prev = NULL : NULL;
 		*head = curr;
 		return (1);
 	}
