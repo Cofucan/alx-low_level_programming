111eba37c19d59b9901ae5a6cf263916b02e012b
cofucan <coolucheofia@gmail.com>
2023-05-18 21:30:36 +0100

    feat: Free a doubly linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a doubly linked list
+ * @head: Pointer to first node in the list.
+ *
+ * Return: Nothing.
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *curr = head;
+	dlistint_t *prev = head;
+
+	while (curr->next)
+	{
+		curr = curr->next;
+		free(prev);
+		prev = curr;
+	}
+	free(curr);
+}
