80f4b589724b4b21dc69e233491bf4e9c7bc5197
cofucan <coolucheofia@gmail.com>
2023-05-18 23:07:14 +0100

    style: Betty compliant

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 
+void insert_middle(dlistint_t ***h, dlistint_t **new_node, dlistint_t **curr);
+
 /**
  * insert_dnodeint_at_index - inserts a node at a given index of a list
  * @h: Pointer to first node in the list.
@@ -26,7 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
-		return NULL;
+		return (NULL);
 
 	new_node->n = n;
 
@@ -34,15 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (index == idx)
 		{ /* Find the node that is currently at the specified index */
-			new_node->next = curr;
-			new_node->prev = curr->prev;
-
-			if (curr->prev)
-				curr->prev->next = new_node;
-			else /* If the current node is the first node */
-				*h = new_node; /* Update the head of the list */
-
-			curr->prev = new_node;
+			insert_middle(&h, &new_node, &curr);
 			return (new_node);
 		}
 
@@ -58,4 +52,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	free(new_node);
 	return (NULL);
+}
+
+/**
+ * insert_middle - inserts a node at a given index in the middle of a list
+ * @h: Pointer to first node in the list.
+ * @new_node: New node to be inserted at index.
+ * @curr: Node that is currently at that index.
+ *
+ * Return: Nothing.
+ */
+
+void insert_middle(dlistint_t ***h, dlistint_t **new_node, dlistint_t **curr)
+{
+	(*new_node)->next = *curr;
+	(*new_node)->prev = (*curr)->prev;
+
+	if ((*curr)->prev)
+		(*curr)->prev->next = *new_node;
+	else /* If the current node is the first node */
+		**h = *new_node; /* Update the head of the list */
+
+	(*curr)->prev = *new_node;
 }
