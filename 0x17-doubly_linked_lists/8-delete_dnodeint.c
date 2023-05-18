01dd2f124274805af15f597271b66b269611003f
cofucan <coolucheofia@gmail.com>
2023-05-19 00:30:16 +0100

    feat: Delete node at index

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes a node at a given index of a list
+ * @head: Address of pointer to first node in the list.
+ * @index: Index of node to be deleted.
+ *
+ * Return: 1 if successful, else -1.
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *curr = *head;
+	unsigned int idx = 0;
+
+	if (*head == NULL) /* Check if list is empty */
+		return (-1);
+
+	if (index == 0)
+	{ /* Delete first node if index is 0 */
+		curr = curr->next;
+		curr ? free(curr->prev) : free(*head);
+		*head = curr;
+		return (1);
+	}
+
+	while (curr)
+	{
+		if (idx == index)
+		{ /* Find the node that is currently at the index */
+			curr->prev->next = curr->next;
+			curr->next->prev = curr->prev;
+			free(curr);
+			return (1);
+		}
+
+		curr = curr->next;
+		idx++;
+	}
+
+	if (idx == index)
+	{ /* Delete last node if index is the last index of the list */
+		curr->prev->next = NULL;
+		free(curr);
+		return (1);
+	}
+
+	return (-1);
+}
