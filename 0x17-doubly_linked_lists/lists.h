d1ee9191c6a514045979c12c70803aa8bddb03fb
cofucan <coolucheofia@gmail.com>
2023-05-18 18:56:36 +0100

    chore: Update header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 #include <unistd.h>
 #include <fcntl.h>
 
@@ -24,5 +25,6 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif /* LIST_H */
