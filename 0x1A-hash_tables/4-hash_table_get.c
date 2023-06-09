4f66611429266301471b2e679954ed8e6003a413
cofucan <coolucheofia@gmail.com>
2023-06-09 20:38:45 +0100

    refactor: Remove unused variables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,6 +1,4 @@
 #include "hash_tables.h"
-#include <string.h>
-
 
 /**
  * hash_table_get - Gets the value associated with a key from the hash table.
@@ -11,24 +9,22 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *ht_pair;
+	hash_node_t *ht_bucket;
 	unsigned long int index;
-	hash_node_t *ht_slot;
-	char *update_value;
 
 	if (!ht || !key || !(*key))
 		return (NULL);
 
 	/* Get the key index and the node at that index */
 	index = key_index((unsigned char *)key, ht->size);
-	ht_pair = ht->array[index];
+	ht_bucket = ht->array[index];
 
 	/* Iterate over the linked list until the key is found */
-	while (ht_pair)
+	while (ht_bucket)
 	{
-		if (strcmp(ht_pair->key, key) == 0)
-			return (ht_pair->value);
-		ht_pair = ht_pair->next;
+		if (strcmp(ht_bucket->key, key) == 0)
+			return (ht_bucket->value);
+		ht_bucket = ht_bucket->next;
 	}
 	return (NULL);
 }
