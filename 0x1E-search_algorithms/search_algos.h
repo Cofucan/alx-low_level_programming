26af97a77cd7f633e35d60e983a5101c40bc6e4d
Uche Ofia <coolucheofia@gmail.com>
2023-09-13 22:41:28 +0100

    chore: Header file with all prototypes added

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef _SEARCH_ALGOS_
+#define _SEARCH_ALGOS_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* _SEARCH_ALGOS_ */
